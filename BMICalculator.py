926b3985802702057e7b730f0330ab72a248c7e3
sadafmey <145181330+sadafmey@users.noreply.github.com>
2024-03-02 19:17:38 +0400

    Update BMICalculator.py

--- BMICalculator.py
+++ BMICalculator.py
@@ -1,3 +1,16 @@
+
+def classify_bmi(bmi):
+ 
+    if bmi < 18.5:
+        return "Underweight"
+    elif bmi < 25:
+        return "Normal weight"
+    elif bmi < 30:
+        return "Overweight"
+    else:
+        return "Obese"
+
+
 def calculate_bmi(weight, height):
   
     bmi = weight / (height ** 2)
@@ -8,7 +21,9 @@ def main():
     weight = float(input("Enter your weight in kilograms: "))
     height = float(input("Enter your height in meters: "))
     bmi = calculate_bmi(weight, height)
+    bmi_category = classify_bmi(bmi)
     print(f"Your BMI is: {bmi:.2f}")
+    print(f"You are classified as: {bmi_category}")
 
 if __name__ == "__main__":
     main()
